fbc491da0536b97a553e060fe344a93df2fdaee1
Julie S Mike <juliesmike@gmail.com>
2017-03-29 17:09:53 -0500

    convert to JSX and add Babel

--- react-practice/index.js
+++ react-practice/index.js
@@ -1,13 +1,27 @@
-const { createElement } = React
+// const { createElement } = React   //Don't need when use JSX
 const { render } = ReactDOM
 
-const title = createElement(
-  'h1',
-  {id: 'title', className: 'header'},
-  'Hello World'
-)
+//use inline instead
+// const style = {
+//   backgroundColor: 'orange',
+//   color: 'white',
+//   fontFamily: 'verdana'
+// }
 
-ReactDOM.render(
-  title,
+//Don't need when use JSX
+// const title = createElement(
+//   'h1',
+//   {id: 'title', className: 'header', style: style},
+//   'Hello World'
+// )
+
+render(
+  <h1 id='title'
+      className='header'
+      style={{backgroundColor: 'orange',
+              color: 'white',
+              fontFamily: 'verdana'}}>
+  Hello World
+  </h1>,
   document.getElementById('react-container')
 )
